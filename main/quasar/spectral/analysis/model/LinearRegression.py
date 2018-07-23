f39c71c4a3da6ba03fbc7d328836178adb881b54
Nathnael Alemu <nalemu@CHIC02VQ106G8WN.sea.corp.expecn.com>
2018-07-23 12:30:59 -0700

    implemented Newton's method

--- main/quasar/spectral/analysis/model/LinearRegression.py
+++ main/quasar/spectral/analysis/model/LinearRegression.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
+import random
 
 
 class LinearRegression:
@@ -11,7 +12,7 @@ class LinearRegression:
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.featureMatrix,self.label,train_ratio)
         self.theta = np.array()
         for i in range(len(self.x_train)):
-            self.theta[i] = 0.5 #random number between 0 and 2
+            self.theta[i] = random.random() #random number between 0 and 1
 
 
 
@@ -26,19 +27,59 @@ class LinearRegression:
 
     def fit_gradient_descent(self):
         num_iter = 1000
-        learning_rate = 1
+        alpha = 1
+        '''
+        err_vector = np.multiply(np.square(np.add(np.matmul(self.theta,self.x_train),self.y_train)),0.5)
+        d_err_vector = np.add(np.matmul(self.theta,self.x_train),self.y_train) --> derivative
+          for certain number of iterations:
+              theta = theta - alpha*derivativeOfErrorWithRespectTheta 
+                   
+        '''
+        d_err_vector = np.add(np.matmul(self.theta, self.x_train), self.y_train)
         for i in range(num_iter):
-            for j in range(len(self.x_train)):#for each example
-                change_in_theta = learning_rate*(self.h(self.x_train[j],self.y_train[j]) - self.y_train[j])*self.x_train[j] #this is a list
-                if(self.thetaHasntConverged(change_in_theta)):
-                    self.updateTheta(change_in_theta)
+            self.theta = np.subtract(self.theta,np.multiply(alpha,d_err_vector))
+            d_err_vector = np.add(np.matmul(self.theta, self.x_train), self.y_train)
+
+
 
 
     def h(self,current_x, current_y):
 
+        current_theta = self.theta
+        f_of_theta_current = np.add(np.matmul(self.x_train,self.theta),self.y_train)
+        while not self.converges(f_of_theta_current):
+            #current_theta = current_theta - f_of_theta_current/f'(theta_current)
 
+        #copy current theta into self.theta
+
+    def converges(self,f_of_theta):
+        threshold=0.5
+        for err in f_of_theta:
+            if err > threshold:
+                return False
+        return True
 
     def fit_newton_method(self):
+        #inputs: Theta, X_train, Y_train
+        # make sure Theta is initialized to random number in constructor
+        # generate threshold
+        # err_vector = Square(multiply(theta,X_train) + Y_train)*1/2
+        # while (errHasConverged(err_vector,threshold)):  //this means every value of err_vector has to be above threshold
+        #    d_err_vector = multiply(theta,X_train) = Y
+        #    self.Theta = Self.theta - inverse(err_vector)*d_err_vector
+        #    err_vector = Square(multiply(theta,X_train) + Y_train)*1/2
+        threshold = 0.0001
+        err_vector = np.multiply(np.square(np.add(np.matmul(self.theta,self.x_train),self.y_train)),0.5)
+        while self.errHasConverged(err_vector,threshold):
+            d_err_vector = np.add(np.matmul(self.theta,self.x_train),self.y_train)
+            self.theta = np.subtract(self.theta,np.multiply(np.linalg.inv(err_vector),d_err_vector))
+            err_vector = np.multiply(np.square(np.add(np.matmul(self.theta,self.x_train),self.y_train)),0.5)
+
+    def errHasConverged(self,err_vector,threshold):
+        for error in err_vector:
+            if error  > threshold:
+                return False
+        return True
 
     def updateTheta(self,theta_change):
 
--- main/quasar/spectral/analysis/model/LocallyWeightedRegression.py
+++ main/quasar/spectral/analysis/model/LocallyWeightedRegression.py
@@ -1,2 +1,12 @@
 
 class LocallyWightedRegression:
+
+'''
+
+Differences between Linear Regression and Locally Weighted Regression:
+     1.Error function = 0.5*Weight(h(x_train) - y_train)**2
+          Wight = e**(x-x_query)**2/learning_rate
+     2. No fitting during init. everytime predict is called, first you call
+        fit with the query param passed, then once that is done, continue prediction
+
+'''
